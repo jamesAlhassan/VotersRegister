06b3cd34261af23e9865fbab408560f9c599f349
James Alhassan <jamesalhassan052@gmail.com>
2023-03-17 08:48:08 +0000

    Add login.html setup

--- vRegister/views.py
+++ vRegister/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def login(request):
+def base(request):
     return render(request, 'base.html')
+
+def login(request):
+    return render(request, 'login.html')
