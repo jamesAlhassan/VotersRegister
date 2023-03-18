448f228b52e4114f520e6554a60bbeec23f1b1b2
James Alhassan <jamesalhassan052@gmail.com>
2023-03-18 15:05:40 +0000

    accounts app setup

--- votersRegister/settings.py
+++ votersRegister/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "vRegister.apps.VregisterConfig"
+    "vRegister.apps.VregisterConfig",
+    "accounts.apps.AccountsConfig"
 ]
 
 MIDDLEWARE = [
