97f97350382e3dfb373b5617611fb4048cd30b6c
James Alhassan <jamesalhassan052@gmail.com>
2023-03-18 14:49:14 +0000

    Update login.html

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+// ********** set date ************
+// select span
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
+
--- vRegister/urls.py
+++ vRegister/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import login
+from .views import login, base
 
 urlpatterns = [
     path("", login, name='login'),
+    path("base/", base, name='base')
 ]
