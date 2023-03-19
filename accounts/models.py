ef53b2a43ae8cd16a26ec2821bf0cb6cc22f89c9
James Alhassan <jamesalhassan052@gmail.com>
2023-03-19 05:46:28 +0000

    u

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+GENDER_CHOICES = (
+        ('M', 'Homme'),
+        ('F', 'Femme'),
+    )
+    
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.PositiveIntegerField(null=False, blank=False)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
--- votersRegister/settings.py
+++ votersRegister/settings.py
@@ -125,3 +125,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "hero"
+LOGOUT_REDIRECT_URL = "hero"
+AUTH_USER_MODEL = "accounts.CustomUser"
