aa30881a9539dae0e13a3a400d877fdbf6aad079
James Alhassan <jamesalhassan052@gmail.com>
2023-03-20 17:58:29 +0000

    Add crispy form tags

--- votersRegister/settings.py
+++ votersRegister/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # 3rd Party
+    "crispy_forms",
+    "crispy_bootstrap5",
     "vRegister.apps.VregisterConfig",
     "accounts.apps.AccountsConfig"
 ]
@@ -127,3 +131,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "hero"
 LOGOUT_REDIRECT_URL = "hero"
 AUTH_USER_MODEL = "accounts.CustomUser"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
